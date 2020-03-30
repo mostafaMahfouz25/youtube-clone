3d0c9c0fb6a4f236ea3b4b3eaa21809d83e5dc8b
eraasoft <eraasoft2019@gmail.com>
2020-03-30 15:55:32 +0200

    Updates

---
+++ precache-manifest.5b9d64b49787f22af3108caff7168c68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37c6d719f15d1e3a6d53411d5f937bc5",
+    "url": "/youtube-clone-app/index.html"
+  },
+  {
+    "revision": "0a3983d1209bf01f3501",
+    "url": "/youtube-clone-app/static/css/main.ad9d2a8f.chunk.css"
+  },
+  {
+    "revision": "120b9997dc5089bb3c47",
+    "url": "/youtube-clone-app/static/js/2.305c7981.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/youtube-clone-app/static/js/2.305c7981.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a3983d1209bf01f3501",
+    "url": "/youtube-clone-app/static/js/main.9dc16c34.chunk.js"
+  },
+  {
+    "revision": "bfc3ef130277ba4f89db",
+    "url": "/youtube-clone-app/static/js/runtime-main.467579e3.js"
+  }
+]);
